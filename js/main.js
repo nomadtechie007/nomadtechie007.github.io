eae4be352c9b38dd963bdc1646cb46a083631cc0
Mukesh chandra <nomadtechie007@@gmail.com>
2019-01-20 01:49:26 +0530

    fixes

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector("#submit_button").onclick(function() {
+  document.querySelector("#name").value = "";
+  document.querySelector("#email").value = "";
+  document.querySelector("#message").value = "";
+});
